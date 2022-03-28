ad88675e7c6ea59ecfe8f0bedbf2ea9ac46ecd54
Rohail Babar <rohailbabar1997@gmail.com>
2022-03-29 02:23:26 +0500

    commit

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers/rootReducer';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 const store = createStore(rootReducer,
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
@@ -13,8 +12,3 @@ const store = createStore(rootReducer,
 
 ReactDOM.render(
 <Provider store={store}><App /></Provider>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
